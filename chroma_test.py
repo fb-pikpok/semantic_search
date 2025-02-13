64bd4be4ef55d34d48ce54a8306c460fba8d6880
fbohm <fbohm@pikpok.com>
2025-02-13 16:13:42 +1300

    First Chroma DB with HRC data up and running

---
+++ chroma_test.py
@@ -0,0 +1,6 @@
+import chromadb
+
+client = chromadb.PersistentClient(path="S:\SID\Analytics\Working Files\Individual\Florian\Projects\semantic_search\Data\ChromaDB")
+
+client.heartbeat()
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import pandas as pd
 root = r'S:\SID\Analytics\Working Files\Individual\Florian\Projects\semantic_search\Data'
 source = r'db_embedded.json'
 
@@ -8,3 +9,7 @@ input = os.path.join(root, source)
 with open(input, "r", encoding="utf-8") as f:
     data = json.load(f)
 
+# Convert the JSON data into a DataFrame
+df = pd.DataFrame(data)
+
+print(df.head())
